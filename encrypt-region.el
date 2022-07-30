775694e030a6400d3e0736f5c75370e145ad68c8
Carlton Shepherd <carlton@linux.com>
2022-07-30 22:56:55 +0100

    Cleanup

--- encrypt-region.el
+++ encrypt-region.el
@@ -1,14 +1,15 @@
-;; Copyright © 2021, Carlton Shepherd
-;; Author: Carlton Shepherd (https://cs.gl)
-;; Version: 0.2
+;; encrypt-region.el -- Encrypts and decrypts regions
+;;
+;; Copyright (c) 2022, Carlton Shepherd
+;; Author: Carlton Shepherd <carlton@linux.com> (https://cs.gl)
+;; Version: 0.1
 ;; Created: 14 October 2021
 ;; Modified: 30 July 2022
-;; Keywords: encryption
+;; Keywords: encryption, cryptography
 ;; License: GPLv3
-;; Homepage: https://github.com/cgshep/encrypt-region
+;; URL: https://github.com/cgshep/encrypt-region
 ;;
-;; Notes: This package allows the encryption and decryption of regions.
-;;        Authenticated encryption is provided by ChaCha20-Poly1305 from GnuTLS.
+;; Notes: Authenticated encryption is provided by GnuTLS' ChaCha20-Poly1305
 ;;
 ;; This file is not part of GNU Emacs.
 ;;
@@ -21,18 +22,11 @@
   :type 'string
   :group 'encrypt-region)
 
-(defcustom encrypt-region--encrypt-buf-name "name"
-  "Define name for outputting encrypted regions."
-  :type 'string
-  :group 'encrypt-region)
-
-(defcustom encrypt-region--decrypt-buf-name "name"
-  "Define name for outputting decrypted regions."
-  :type 'string
-  :group 'encrypt-region)
+(defvar encrypt-region--encrypt-buf-name "*Encrypt Region*")
+(defvar encrypt-region--decrypt-buf-name "*Decrypt Region*")
 
 (defun encrypt-region--pad (input length)
-  "Pad string to a given length."
+  "Pad string to a given length using PKCS#7."
   ;; Thanks to auth-source.el
   ;; https://github.com/emacs-mirror/emacs/blob/master/lisp/auth-source.el
   (let ((p (- length (mod (length input) length))))
@@ -42,25 +36,6 @@
   "Remove padding from string."
   (substring string 0 (- (length string)
 			 (aref string (1- (length string))))))
-
-(defun decrypt-region (start end)
-  "Decrypt a base64-encoded encrypted region."
-  (interactive "r")
-  (let ((ctext-str (split-string
-		    (buffer-substring start end) "###")))
-    (with-output-to-temp-buffer
-	(generate-new-buffer encrypt-region--buf-name)
-      (prin1 (encrypt-region--unpad
-	      (decode-coding-string
-	       (gnutls-symmetric-decrypt "CHACHA20-POLY1305"
-					 (copy-sequence encrypt-region--key)
-	                                 ; Decode the IV
-					 (base64-decode-string (cadr ctext-str))
-					 ; Decode the ciphertext
-					 (base64-decode-string (car ctext-str)))
-	       'utf-8)))
-      ; Output to the decrypt temporary buffer
-      (switch-to-buffer encrypt-region--decrypt-buf-name))))
      
 (defun encrypt-region (start end)
   "Encrypts a region and outputs its base64 encoding."
@@ -76,6 +51,26 @@
 				       (encode-coding-string (buffer-substring start end) 'utf-8)
 				       64)
 				      "") "###"))
-    (switch-to-buffer encrypt-region--buf-name)))
+    (switch-to-buffer encrypt-region--encrypt-buf-name)))
+
+(defun decrypt-region (start end)
+  "Decrypt a base64-encoded encrypted region."
+  (interactive "r")
+  (let ((ctext-str (split-string
+		    (buffer-substring start end) "###")))
+    (with-output-to-temp-buffer
+	(generate-new-buffer encrypt-region--decrypt-buf-name)
+      (prin1 (encrypt-region--unpad
+	      (decode-coding-string
+	       (gnutls-symmetric-decrypt "CHACHA20-POLY1305"
+					 (copy-sequence encrypt-region--key)
+	                                 ; Decode the IV
+					 (base64-decode-string (cadr ctext-str))
+					 ; Decode the ciphertext
+					 (base64-decode-string (car ctext-str))))
+	       'utf-8))
+      ; Output to the decrypt temporary buffer
+      (switch-to-buffer encrypt-region--decrypt-buf-name))))
 
 (provide 'encrypt-region)
+;; encrypt-region.el ends here
